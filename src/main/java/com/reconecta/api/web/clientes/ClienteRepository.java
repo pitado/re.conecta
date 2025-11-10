429e3b44caa47aaf813f940b1d07b4c87352a149
Miguel Pita <Miguellpitaa@gmail.com>
2025-11-10 14:50:38 -0300

    Delete src/main/java/com/reconecta/api/web directory

--- src/main/java/com/reconecta/api/web/clientes/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.reconecta.api.web.clientes;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-    boolean existsByEmailIgnoreCase(String email);
-}
